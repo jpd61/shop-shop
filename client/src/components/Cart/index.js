93a2dd4e1316fef4c7344298c62b8396f1b68ec2
jpd61 <jpd@dewoodyoil.com>
2020-08-16 12:36:04 -0500

    install redux

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/react-hooks';
+import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
